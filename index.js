d8a44eef8c7f6c555953d7cbfa7cc5d16ab4a31c
Leejere <lijfujian@gmail.com>
2022-12-05 16:32:13 -0500

    test

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-import express from 'express';
+const express = require('express');
+const { Pool } = require('pg');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 app.get('/', (req, res) => {
   res.send('Hello from your server');
+})
+.get('/db', async(req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM trips LIMIT 5');
+    const results = { 'results': (result) ? result.rows : null };
+    res.send(results);
+  } catch(err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
 });
 
 app.listen(port, () => {
