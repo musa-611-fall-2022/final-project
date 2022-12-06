92c2a774be9cca8c4464cdcf39c0dade31bdfc9a
MicahEpstein <mepstein@risd.edu>
2022-12-05 22:50:07 -0500

    mapped scroll to time interval but also kinda broke it

--- site/js/main.js
+++ site/js/main.js
@@ -2,22 +2,79 @@
 //import data and functions
 import { initializeMap} from './map.js';
 
+function fetchData() {
+  fetch(`./data/311data.csv`)
+    .then(resp => resp.text())
+    .then(data => {
+      const dataset = Papa.parse(data, { header: true });
+      
+      const cleandata = [];
+      //for loop that cleans the dataset
+      for (const element of dataset.data){
+        if (element['lat'] !== "NA" && element['lat'] !== undefined && element['lat'] !== ''){
+          element.parsedInterval = Date.parse(element["interval15"]);
+          //make new column called simpleInterval from 0-960 for each interval
+         //element.parsedInterval960 = element['simpleInterval']
+          cleandata.push(element);
+        }
+      }
+      window.dataset = cleandata;
+      //creating a map from parsedInterval to an array of indices (i) in cleandata
+      //this needs to map from simpleInterval instead
+      const dataDic = {};
+      for (let i = 0; i < cleandata.length; i++){
+        const cleanElement = cleandata[i];
+        if (!dataDic[cleanElement.parsedInterval]){
+          dataDic[cleanElement.parsedInterval] = [i];
+        } else {
+          dataDic[cleanElement.parsedInterval].push(i);
+        }
+      }
+      window.dataDic = dataDic;
+    });
+}
 
 let map = initializeMap();
+fetchData();
 
 /*function onScroll() {
     var counter = document.getElementById("scrollValue");
     counter.innerHTML = document.documentElement.scrollTop;
 }*/
 
+let minScroll = 0;
+let maxScroll = 100;
+let minInt = 0;
+let maxInt = 960;
+let minMs = 1666843200000;
+let maxMs = 1667707200000;
+let proportion = (maxMs - minMs)/(maxScroll-minScroll);
+let proportion2 = (maxInt - minInt)/(maxScroll-minScroll);
+
+function scrollToDate(scrolled){
+  return Math.floor(minMs + (proportion * (scrolled - minScroll)));
+}
+
+function scrollToDate2(scrolled){
+  return Math.floor(minInt + (proportion2 * (scrolled - minScroll)));
+}
+
 function progressBarScroll() {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
         height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
         scrolled = (winScroll / height) * 100;
     document.getElementById("progressBar").style.width = scrolled + "%";
-    console.log(winScroll);
-    let counter = Math.round(winScroll)
-    fetchData(counter);
+    let scrollMs = scrollToDate(scrolled);
+    console.log("you have scrolled " + scrolled + '%');
+    console.log("The ms you are on is " + scrollMs);
+    //if (window.dataDic[scrollSimple]){
+    if (window.dataDic[scrollMs]){
+      let indicesToAdd = window.dataDic[scrollMs];
+      for (const index of indicesToAdd){
+        addToMap(window.dataset[index]);
+      }
+    }
+  
   }
 
   function make311Feature(data){
@@ -34,26 +91,18 @@ function progressBarScroll() {
     };
   }
 
-  function fetchData(counter) {
-    fetch(`./data/311data.csv`)
-      .then(resp => resp.text())
-      .then(data => {
-        const dataset = Papa.parse(data, { header: true });
-        window.data = data;
-          //if tigerline != empty, then run the following (to protect against empty attributes)
-          if (dataset.data[counter]['lat'] !== "NA" && dataset.data[counter]['lat'] !== undefined && dataset.data[counter]['lat'] !== ''){
-          const dataFeature = make311Feature(dataset.data[counter]);
-          //console.log(dataFeature.properties["parsed-interval"]);
-          console.log(dataFeature);
-          map.dataLayer.addData(dataFeature);
-          }
-      });
+function addToMap(dataToAdd) {
+    const dataFeature = make311Feature(dataToAdd);
+    //console.log(dataFeature.properties["parsed-interval"]);
+    console.log(dataFeature);
+    map.dataLayer.addData(dataFeature);
   }
   
   window.onscroll = function () {
     progressBarScroll();
-  
   };
+
+
 
   
   var options = {
