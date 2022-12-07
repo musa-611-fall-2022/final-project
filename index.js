b69bfc83041aad7fcf266a6281ad217a598d5f80
Leejere <lijfujian@gmail.com>
2022-12-07 15:59:33 -0500

    test send html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { Pool } = require('pg');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,20 +12,22 @@ let pool = new Pool({
   },
 });
 
-app.get('/', (req, res) => {
-  res.render('site/index.html');
-})
-.get('/test-query', async(req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT trip_start_time, COUNT (trip_start_time) FROM trips WHERE primary_mode = 3 GROUP BY trip_start_time');
-    const results = { 'results': (result) ? result.rows : null };
-    res.send(results);
-  } catch(err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
-});
+app
+  .use(express.static(path.join(__dirname, 'site')))
+  .get('/', (req, res) => {
+    res.sendFile('index.html');
+  })
+  .get('/test-query', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT trip_start_time, COUNT (trip_start_time) FROM trips WHERE primary_mode = 3 GROUP BY trip_start_time');
+      const results = { 'results': (result) ? result.rows : null };
+      res.send(results);
+    } catch(err) {
+      console.error(err);
+      res.send('Error ' + err);
+    }
+  });
 
 app.listen(port, () => {
   console.log(`API listening on port ${port}`);
