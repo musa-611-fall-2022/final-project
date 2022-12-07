c3303dea35eb00135422a7c0d2c523335d1db9b5
Leejere <lijfujian@gmail.com>
2022-12-07 15:45:05 -0500

    test render index.html

--- index.js
+++ index.js
@@ -4,31 +4,15 @@ const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 5000;
 
-let pool;
-
-if(process.env.DATABASE_URL) {
-  // If on Heroku
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-} else {
-  // If on local
-  pool = new Pool({
-    connectionString: 'postgres://wirkarnsvpjngv:e991ee89cbd9822ac73be40d270229c72a3040fa094a9bbbdb452ef1074fb7ee@ec2-3-234-131-8.compute-1.amazonaws.com:5432/d3rh3ac7evu96k',
-    user: 'wirkarnsvpjngv',
-    password: 'e991ee89cbd9822ac73be40d270229c72a3040fa094a9bbbdb452ef1074fb7ee',
-    port:'5432',
-    host: 'ec2-3-234-131-8.compute-1.amazonaws.com',
-    database: 'd3rh3ac7evu96k',
-    sslmode: 'require',
-  });
-}
+let pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello from your server');
+  res.render('site/index.html');
 })
 .get('/test-query', async(req, res) => {
   try {
