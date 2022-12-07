3fb81581c482acd57f5cfc61b129a53181d4ae44
Jonathon Sun <jonathonsun03@gmail.com>
2022-12-07 15:29:25 -0500

    Everything works again going through Mjumbe's fixes

--- site/js/dataContext.js
+++ site/js/dataContext.js
@@ -1,8 +1,9 @@
 function loadResearchData(currentSlideIndex) {
+  
   if(currentSlideIndex == 3) {
     L.geoJSON(eastasiancluster, {
       style: {fill: null, color: 'black'},
-    }).addTo(map);
+    }).addTo(layerGroup);
     };
   else if(currentSlideIndex == 4) {
     L.geoJSON(southasiancluster, {
