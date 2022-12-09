50a19260faeae28792ad632f4eaa3615c2e658ad
Leejere <lijfujian@gmail.com>
2022-12-08 22:40:28 -0500

    test

--- site/js/main.js
+++ site/js/main.js
@@ -1,12 +1,25 @@
-fetch(`https://mobiladelphia.herokuapp.com/test-query/3`)
-.then(resp => {
-  if(resp.status === 200) {
-    const data = resp.json();
-    return data;
-  } else {
-    //
-  }
-})
-.then(data => {
-  console.log(data);
-});
+// fetch(`https://mobiladelphia.herokuapp.com/test-query/3`)
+// .then(resp => {
+//   if(resp.status === 200) {
+//     const data = resp.json();
+//     return data;
+//   } else {
+//     //
+//   }
+// })
+// .then(data => {
+//   console.log(data);
+// });
+
+/* =================================================
+Main.js deals with the actions that function on global scale,
+defines global objects,
+and is a coordinator of all different modules
+
+@auther: Jie Li
+@date: 12/08/2022
+================================================= */
+
+import { test } from "./display-map.js";
+
+test();
