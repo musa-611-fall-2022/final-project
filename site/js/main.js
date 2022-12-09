2920eedc4702fd8f27e115f5a3ba906673e9590f
Leejere <lijfujian@gmail.com>
2022-12-09 12:54:57 -0500

    test

--- site/js/main.js
+++ site/js/main.js
@@ -1,15 +1,15 @@
-fetch(`https://mobiladelphia.herokuapp.com/test-query/3`)
-.then(resp => {
-  if(resp.status === 200) {
-    const data = resp.json();
-    return data;
-  } else {
-    //
-  }
-})
-.then(data => {
-  console.log(data);
-});
+// fetch(`https://mobiladelphia.herokuapp.com/test-query/3`)
+// .then(resp => {
+//   if(resp.status === 200) {
+//     const data = resp.json();
+//     return data;
+//   } else {
+//     //
+//   }
+// })
+// .then(data => {
+//   console.log(data);
+// });
 
 /* =================================================
 Main.js deals with the actions that function on global scale,
@@ -26,3 +26,39 @@ import { initMap, addBlockGroups } from "./display-map.js";
 const map = initMap();
 
 addBlockGroups(map);
+
+/* ==========
+Objects to store params for API
+=========== */
+
+// Toggle display params
+export const toggleDisplayParam = {
+  groupBy: "origin_geoid", // default to departures
+  displayVar: "count",
+}
+
+// Filter params
+export const filterParams = {
+  categoricalVars: [
+    {
+      varName: "primary_mode",
+      filteredCategories: [1, 2, 3, 4, 5, 6, 7],
+    },
+    {
+      varName: "trip_purpose",
+      filteredCategories: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+  ],
+  continuousVars: [
+    {
+      varName: "trip_taker_age",
+      lowerBound: 0,
+      upperBound: 100,
+    },
+    {
+      varName: "trip_taker_income",
+      lowerBound: 0,
+      upperBound: 1000000000,
+    }
+  ]
+}
--- site/js/navigation.js
+++ site/js/navigation.js
@@ -1,4 +1,4 @@
-/* Drop downs */
+/* ======= Drop downs || Make all dropdowns work ======= */
 
 const allDropdownButtonsEls = document.querySelectorAll(".dropdown-button");
 
@@ -7,22 +7,67 @@ for(let dropdownButtonEl of allDropdownButtonsEls) {
   dropdownButtonEl.open = false;
 
   dropdownButtonEl.addEventListener("click", ( ) => {
+
+    // The dropdown content corresponding to this button
     const dropdownContentEl = dropdownButtonEl.nextElementSibling;
 
     if(!dropdownButtonEl.open) { // currently closed
+      // Make it exist
       dropdownContentEl.style.display = "flex";
+      // Transition height
       setTimeout(() => {
         dropdownContentEl.style.maxHeight = "10rem";
         dropdownContentEl.style.padding = "0.5rem";
       }, 0);
+      // Update state
       dropdownButtonEl.open = true;
-    } else {
+    } else { // currently open
+      // Transition height
       dropdownContentEl.style.maxHeight = "0";
       dropdownContentEl.style.padding = "0 0.5rem";
+      // Remove
       setTimeout(() => {
         dropdownContentEl.style.display = "none";
       }, 280);
+      // Update state
       dropdownButtonEl.open = false;
     }
   });
+}
+
+/* ======= Checkboxes =========== */
+
+// Unchecks all options of .cb-invisible type
+// inside of a checkbox group of .cb-group class
+function uncheckAllOptions(cbsEls) {
+  for (const cbEl of cbsEls) {
+    cbEl.checked = false;
+  }
+}
+
+// Makes options mutually exclusive
+// Takes checkbox groups of .cb-group class
+// Adds features; does not return
+function makeOptionsExclusive(cbGroupEl) {
+  // Select all options
+  const cbsEls = cbGroupEl.getElementsByClassName("cb-invisible");
+  for(const cbEl of cbsEls) {
+    cbEl.addEventListener("click", () => {
+      if(cbEl.checked === true) {
+        // Meaning this option is to be checked
+        // Uncheck all options before checking this one
+        uncheckAllOptions(cbsEls);
+        cbEl.checked = true;
+      } else {
+        cbEl.checked = false;
+      }
+    });
+  }
+}
+
+// Make all checkbox groups of .cb-group class
+// have options of .cb-invisible class mutually exclusive
+const allCbGroupsEls = document.querySelectorAll(".cb-group");
+for(const cbGroupEl of allCbGroupsEls) {
+  makeOptionsExclusive(cbGroupEl);
 }
