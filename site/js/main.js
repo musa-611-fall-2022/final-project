a76490e44c0363cf5ff69be603c98b5bd32f70f5
miaomiao612 <dddoctorr612@gmail.com>
2022-12-13 11:27:11 +0800

    update

--- site/js/main.js
+++ site/js/main.js
@@ -2,7 +2,7 @@
  * @Author: miaomiao612 dddoctorr612@gmail.com
  * @Date: 2022-12-10 06:41:25
  * @LastEditors: miaomiao612 dddoctorr612@gmail.com
- * @LastEditTime: 2022-12-11 04:06:38
+ * @LastEditTime: 2022-12-13 11:18:31
  * @FilePath: \final-project\site\js\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,7 +18,7 @@ let directionButton = document.querySelector('#direction');
 function hidePopUp() {
   let popup = document.getElementById("popUp");
   let mapAfter = document.getElementById("map");
-  popup.classList.remove("open-popUp"); 
+  popup.classList.remove("open-popUp");
   mapAfter.classList.remove("mapAfter");
 }
 
@@ -62,7 +62,7 @@ function setupGeolocationEvent() {
   );
 }
 
-  
+
 function fakePosition(map) {
     const pos={
         "type": "Feature",
@@ -94,8 +94,11 @@ function getDirection() {
 }
 
 
+
+
+
 //Execute functions
-//setupGeolocationEvent();
+setupGeolocationEvent();
 fakePosition(map);
 Search(map, search, facilities);
 getDirection();
