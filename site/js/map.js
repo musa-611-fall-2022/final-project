1889991f7a89800015e4de474d854b63de48ac20
Morgangriff12 <112583600+Morgangriff12@users.noreply.github.com>
2022-12-19 10:19:07 -0500

    first attempt to pass tests

--- site/js/map.js
+++ site/js/map.js
@@ -10,13 +10,13 @@ function initializeSiteMap() {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     }).addTo(siteMap);
 
+    // eslint-disable-next-line no-unused-vars
     const boundaries = L.geoJSON(somervilleBoundaries, {
       style: { fill:null, color: 'whitesmoke' },
     }).addTo(siteMap);
 
     return siteMap;
   }
-
 
 function makeSiteFeature(site)  {
   return {
@@ -46,35 +46,28 @@ function showSitesOnMap(sitesToShow, siteMap) {
 };
 
 // Will try to figure out how to use the following code to make different colored leaflet markers if time!
-//function featureStyle(feature) {
- // const challengeColors = {
-    //"Ghost Ads": "#217e79",
-    //"Murals": "#008096",
-   // "Monuments": "#237DAD",
-    //"Reliefs": "#6475B4",
-   // "Historic Houses": "#9769A8",
-   // "Historic Curios": "#BA5E8A",
- // };
-  //const challengeFeature = feature.properties["challenge"];
-  //return {
-   // fillColor: challengeColors[challengeFeature]
-   // stroke: true,
-   // color: '#000000',
-    //weight: 1,
-   // fillOpacity: 1,
-    //radius: 6}
-//};
+function featureStyle(feature) {
+  const challengeColors = {
+    "Ghost Ads": "#217e79",
+    "Murals": "#008096",
+    "Monuments": "#237DAD",
+    "Reliefs": "#6475B4",
+   "Historic Houses": "#9769A8",
+   "Historic Curios": "#BA5E8A",
+ };
+  const challengeFeature = feature.properties["challenge"];
+  return {
+   fillColor: challengeColors[challengeFeature],
+   stroke: true,
+   color: '#000000',
+   weight: 1,
+   fillOpacity: 1,
+    radius: 6 };
+}
 
 siteMap.siteLayers = L.geoJSON(siteFeatureCollection, {
   pointToLayer:(geoJsonPoint, latlng) => L.circleMarker(latlng),
-  style: {
-    fillColor: '#6475B4',
-    stroke: true,
-    color: 'whitesmoke',
-    weight: 1,
-    fillOpacity: 1,
-    radius: 6,
-    },
+  style: featureStyle,
   onEachFeature: function(feature, layer) {
     layer.bindPopup('<img src='+ feature.properties.image+' + class= "popup-image" >' +
     "<b>Challenge:</b> " + feature.properties.challenge +
