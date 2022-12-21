7ecbb1898ad6ceeff46ec9c14fb9c87eb9ee6dcd
kmu973 <kmu973@Naver.com>
2022-12-21 15:17:50 -0500

    Update test_wcag.js

--- __tests__/test_wcag.js
+++ __tests__/test_wcag.js
@@ -4,4 +4,8 @@ beforeAll(async () => {
   await page.goto('http://localhost:8080/site/');
 });
 
-
+describe('the index.html page', () => {
+  it('should not have any obvious accessibility violations', async () => {
+    await expect(page).toPassAxeTests();
+  });
+});
