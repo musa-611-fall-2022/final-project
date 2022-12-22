7a49bd9dc86eda8bcad37f8c14b9965efe89dc98
sim-py <98434506+sim-py@users.noreply.github.com>
2022-12-21 20:01:34 -0500

    final update?

--- site/js/map.js
+++ site/js/map.js
@@ -1,30 +1,27 @@
 import neighborhoods from "../data/neighborhoods.js";
 
 function initMap() {
-  let map = L.map('map', { maxZoom: 22, preferCanvas: true }).setView([39.95, -75.16], 15);
-  L.tileLayer(`https://api.mapbox.com/styles/v1/simran-aro-map/clbcy72bk001615nu08334s8v/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2ltcmFuLWFyby1tYXAiLCJhIjoiY2xhdTJlMm9yMDI1ZTN4cHJvZW51Nno4NCJ9.rcqye_8iI_wAlqbcNVTlog&zoomwheel=true&fresh=true#15.52/39.946911/-75.157241
-  `, {
-    maxZoom: 22,
-    attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
-  }).addTo(map);
+  let map = L.map("map", { maxZoom: 22, preferCanvas: true }).setView(
+    [39.95, -75.16],
+    15,
+  );
+  L.tileLayer(
+    `https://api.mapbox.com/styles/v1/simran-aro-map/clbcy72bk001615nu08334s8v/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2ltcmFuLWFyby1tYXAiLCJhIjoiY2xhdTJlMm9yMDI1ZTN4cHJvZW51Nno4NCJ9.rcqye_8iI_wAlqbcNVTlog&zoomwheel=true&fresh=true#15.52/39.946911/-75.157241
+  `,
+    {
+      maxZoom: 22,
+      attribution:
+        '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+    },
+  ).addTo(map);
 
   const phl = L.geoJSON(neighborhoods, {
-    style: { fill: false, color: '#127782', weight: 1 },
+    style: { fill: false, color: "#127782", weight: 1 },
   }).addTo(map);
 
   map.fitBounds(phl.getBounds());
 
   return map;
-}
-
-function makeStopFeature(stop) {
-  return {
-    'type': 'Feature',
-    'properties': {
-      'stopname': stop['stopname'],
-    },
-    'geometry': stop['geometry'],
-  };
 }
 
 function showStopOnMap(stopToShow, map) {
@@ -33,8 +30,8 @@ function showStopOnMap(stopToShow, map) {
   }
 
   const stopFeatureCollection = {
-    'type': 'FeatureCollection',
-    'features': stopToShow.map(makeStopFeature),
+    type: "FeatureCollection",
+    features: stopToShow,
   };
 
   map.stopLayers = L.geoJSON(stopFeatureCollection, {
@@ -44,23 +41,26 @@ function showStopOnMap(stopToShow, map) {
       fillColor: "red",
       weight: 0.255,
       opacity: 1,
-      fillOpacity: 0.8
+      fillOpacity: 0.8,
     },
+    onEachFeature: function (feature, layer) {
+      layer.bindTooltip(`STOP: ${feature.properties.stopname}`);
+    },
+  }).on('click', function (e) {
+    console.log(e.latlng.lat, e.latlng.lng)
+    map.removeLayer(map.stopLayers);
+    let circle = L.circle([e.latlng.lat, e.latlng.lng], {
+      color: "red",
+      fillColor: "#f03",
+      fillOpacity: 0.1,
+      radius: 200,
+    }).on("click", function () {
+      map.stopLayers.addTo(map);
+      map.removeLayer(circle);
+    })
+      .addTo(map);
   })
-    .bindTooltip(layer => layer.feature.properties['name'])
     .addTo(map);
-}
-
-function makeFoodFeature(food) {
-  return {
-    'type': 'Feature',
-    'properties': {
-      'name': food['name'],
-      'amenity': food['amenity'],
-      'cuisine': food['cuisine'],
-    },
-    'geometry': food['geometry'],
-  };
 }
 
 function showFoodOnMap(foodToShow, map) {
@@ -69,8 +69,8 @@ function showFoodOnMap(foodToShow, map) {
   }
 
   const foodFeatureCollection = {
-    'type': 'FeatureCollection',
-    'features': foodToShow.map(makeFoodFeature),
+    type: "FeatureCollection",
+    features: foodToShow,
   };
 
   map.foodLayers = L.geoJSON(foodFeatureCollection, {
@@ -81,20 +81,14 @@ function showFoodOnMap(foodToShow, map) {
       color: "blue",
       weight: 1,
       opacity: 1,
-      fillOpacity: 0.8
+      fillOpacity: 0.8,
+    },
+    onEachFeature: function (feature, layer) {
+      layer.bindTooltip(`Name: ${feature.properties.name}`);
     },
   })
-
-    .bindTooltip(layer => layer.feature.properties['name'])
     .addTo(map);
+
 }
 
-
-
-export {
-  initMap,
-  showFoodOnMap,
-  showStopOnMap,
-};
-
-
+export { initMap, showFoodOnMap, showStopOnMap };
